18da0bd9c8eb1fdfe70e371b59aea30030258436
syliang.24 <carlyleliang@gmail.com>
2017-06-13 14:54:25 -0400

    Create 69. Sqrt-x.java

--- JAVA/69. Sqrt-x.java
+++ JAVA/69. Sqrt-x.java
@@ -19,3 +19,19 @@ public class Solution {
 // why not multiplication? because it will over flow the int length
 // left = mid + 1 && right = mid - 1, this preforms good.
 // the while loop limit ans is right floored the sqrt(x)
+
+// 374. Guess Number Higher or Lower
+// exactly same idea
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int left = 1, right = n;
+        while(left < right) {
+            int mid = left + (right - left)/2;
+            int temp = guess(mid);
+            if (temp == 0) return mid;
+            else if (temp > 0) left = mid + 1;
+            else right = mid - 1;
+        }
+        return left;
+    }
+}
