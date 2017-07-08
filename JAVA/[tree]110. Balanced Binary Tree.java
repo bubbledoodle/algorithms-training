c8e01d90f51811df1ed53a2d1a6028a8ceeb466e
syliang.24 <carlyleliang@gmail.com>
2017-07-08 10:45:21 -0400

    Update [tree]110. Balanced Binary Tree.java

--- JAVA/[tree]110. Balanced Binary Tree.java
+++ JAVA/[tree]110. Balanced Binary Tree.java
@@ -22,6 +22,8 @@ For this problem, a height-balanced binary tree is defined as a binary tree in w
  *     TreeNode(int x) { val = x; }
  * }
  */
+
+/* O(n^2)
 public class Solution {
     public boolean isBalanced(TreeNode root) {
         if (root == null) return true;
@@ -33,3 +35,24 @@ public class Solution {
         return Math.max(depth(root.left), depth(root.right)) + 1;
     }
 }
+
+*/
+
+// O(n) bottom-up method usingi Depth First Search (DFS) tree
+// notice to compute and store the value into variable, in order to avoid further recursion.
+public class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return dfsHeight(root) != -1;
+    }
+    public int dfsHeight(TreeNode root) {
+        if (root == null) return 0;
+        int leftHeight = dfsHeight(root.left);
+        int rightHeight = dfsHeight(root.right);
+        if ( leftHeight == -1) return -1;
+        if ( rightHeight == -1) return -1;
+        
+        if (Math.abs(leftHeight - rightHeight) > 1) return -1; // ensure those enter this step's right and left are balanced. 
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+}
+
